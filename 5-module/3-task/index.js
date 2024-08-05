3b9a93e3daf229e8f7f76df057d7bfe93738b0fb
makaka <michaeltheclue@gmail.com>
2024-08-05 18:26:32 +0300

    сделал 5 модуль

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,21 @@
 function initCarousel() {
-  // ваш код...
+  const carousel = document.querySelector(".container"),
+    carouselRight = carousel.querySelector(
+      ".carousel__arrow_right"),
+   carouselLeft = carousel.querySelector(".carousel__arrow_left"),
+    carouselInner = carousel.querySelector(".carousel__inner");
+  let index = 0;
+  const transform = (y) =>
+    (carouselInner.style.transform = `translateX(-${
+      carouselInner.querySelector(".carousel__slide").offsetWidth * y}px`);
+ carouselLeft.style.display = "none";
+  carousel.onclick = (x) => {
+    if (x.target.closest(".carousel__arrow_right")) {
+      transform(++index);}
+    if (x.target.closest(".carousel__arrow_left")) {
+      transform(--index);}
+    carouselRight.style.display =
+      index < carouselInner.children.length - 1 ? "" : "none";
+   carouselLeft.style.display = index > 0 ? "" : "none";
+  };
 }
