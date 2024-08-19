901114d043254d2918422d56c6d7e91933d8afef
makaka <michaeltheclue@gmail.com>
2024-08-19 18:31:11 +0300

    сделал 9 модуль

--- 9-module/1-task/index.js
+++ 9-module/1-task/index.js
@@ -1,7 +1,6 @@
 export default function promiseClick(button) {
   return new Promise(function(resolve) {
     button.addEventListener('click', (event) => {
-      resolve(event);
-    }, {once: true});
+      resolve(event);}, {once: true});
   });
 }
